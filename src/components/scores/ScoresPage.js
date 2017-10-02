731eb40c2449faab5eab18cfd9cc6e329ba26ae0
Aneudy Guzman <aneguzman@gmail.com>
2017-10-02 01:39:23 -0400

    -Adding pages controllers.

---
+++ src/components/scores/ScoresPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ScoresPage extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>Scores</h1>
+            </div>
+        ); 
+    }
+}
---
+++ src/components/stats/StatsPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class StatsPage extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>Stats</h1>
+            </div>
+        ); 
+    }
+}
+
+export default StatsPage;
