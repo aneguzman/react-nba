0a1003e8e8b8e537fbb4480dce67a61addd3bf61
Aneudy Guzman <aneguzman@gmail.com>
2017-11-01 02:18:04 -0400

    Adding responsive style to news page. Adding masonry to apply better style to feeds.

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import { Row, Col } from 'elemental';
+import { Col } from 'elemental';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 import './news.css';
 
 const News = props => (
-  <div className="feed">
-    <Row>
-      <img src={props['media:content']._attributes.url} alt="" />
-      <Col className="feed-date">
-        <span >{moment(props.pubDate._text).format('Do MMMM YYYY')}</span>
-      </Col>
-      <Col className="feed-title" sm="1">
-        <h2>{props.title._text}</h2>
-      </Col>
-    </Row>
-  </div>
+  <Col className="feed" md="32%" sm="100%">
+    <img src={props['media:content']._attributes.url} alt="" />
+    <Col className="feed-date">
+      <span >{moment(props.pubDate._text).format('Do MMMM YYYY')}</span>
+    </Col>
+    <Col className="feed-title">
+      <h2>{props.title._text}</h2>
+    </Col>
+  </Col>
 );
 
 News.propTypes = {
--- src/components/news/NewsList.js
+++ src/components/news/NewsList.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Masonry from 'react-masonry-component';
+import { Row } from 'elemental';
 import News from './News';
 
-const NewsList = props => (
-  <div className="feed-container">
-    {props.news.map(n => <News key={n.guid._text} {...n} />)}
-  </div>
-);
+const NewsList = props => {
+  const newsListItems = props.news.map(n => <News key={n.guid._text} {...n} />);
+  return (
+    <Row className="feed-container">
+      <Masonry
+        className={'my-gallery-class'}
+        disableImagesLoaded={false}
+        updateOnEachImageLoad={false}
+      >
+        {newsListItems}
+      </Masonry>
+    </Row>
+  );
+};
 
 NewsList.propTypes = {
   news: PropTypes.arrayOf(PropTypes.object).isRequired,
